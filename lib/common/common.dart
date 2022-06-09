ef7d73ed74acd5ec39fbd0500712d43e23eada2a
sokkun <sokkun.norn2019@gmail.com>
2022-06-09 10:23:17 +0700

    feat(init): init first project

---
+++ lib/common/common.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+const Color kTextColor = Colors.black87;
+const MaterialColor kPrimaryColor = MaterialColor(_kPrimaryColor, <int, Color>{
+  50: Color(0xFFFEE1E2),
+  100: Color(0xFFFDB3B7),
+  200: Color(0xFFFB8088),
+  300: Color(0xFFF94D58),
+  400: Color(0xFFF82734),
+  500: Color(_kPrimaryColor),
+  600: Color(0xFFF6010E),
+  700: Color(0xFFF5010C),
+  800: Color(0xFFF30109),
+  900: Color(0xFFF10005),
+});
+const int _kPrimaryColor = 0xFFF70110;
+const Color kGreyBlack = Color(0xFF2B2D2F);
+const Color kBorderColor = Color(0xFFD2D2D2);
+const Color khintColor = Color(0xFF878F94);
+const Color kCardBorderColor = Color(0xFFF8F8F8);
+const Color kBrandingRedColor = Color(0xFFFBEBEC);
+const Color kRedColor = Color(0xFFD93840);
+const Color kDividerColor = Color(0xFFF2F2F2);
+const Color kGrey500 = Color(0xFF9E9E9E);
+const Color kCardBackgroundColor = Color(0xFFF2F2F2);
+const Color kGreyColor100 = Color(0xFFF2F2F2);
+const Color kGreyDarkColor100 = Color(0xFFF8F8F8);
+const Color kErrorBrownColor = Color(0xFFF9CDCC);
+const Color kCardBgColorAlpha67 = Color(0xA7F8F8F8);
+
+const MaterialColor kAccentColor = MaterialColor(_kAccentColor, <int, Color>{
+  100: Color(0xFFFFFFFF),
+  200: Color(_kAccentColor),
+  400: Color(0xFFFFB2B2),
+  700: Color(0xFFFF9899),
+});
+const int _kAccentColor = 0xFFFFE5E5;
+const double kButtonBorderRadius = 8.0;
+const double kCardBorderRadius = 12.0;
+const double kBorderRadius16 = 16.0;
+const EdgeInsets kAppBarActionPadding =
+    EdgeInsets.symmetric(horizontal: 8, vertical: 36);
