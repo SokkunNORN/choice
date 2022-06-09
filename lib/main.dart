3715db2cca15281e7b61cb3c7efdd8ca4671ecfa
sokkun <sokkun.norn2019@gmail.com>
2022-06-09 10:39:49 +0700

    feat(view): remove debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Choice',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(
           primarySwatch: kPrimaryColor,
